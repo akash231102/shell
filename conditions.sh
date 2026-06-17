fee4f4958170f51a300af137d283a0aedc2842cf
akash <bugataakash@gmail.com>
2026-06-17 20:09:24 +0530

    main

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ number=$1
 
 if [ $number -lt 10 ]; then
     echo "$number is less than 10"
-elif [ $number -eq 0 ]; then
+elif [ $number -eq 10 ]; then
     echo "$number is equal to 10"
 else 
     echo "$number is greated than 10"
