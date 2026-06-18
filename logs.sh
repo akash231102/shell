4e269d940fcb317e107b9d5587d866357ad198c4
akash <bugataakash@gmail.com>
2026-06-18 10:36:55 +0530

    main

---
+++ logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+log_folder=/var/log/shell-script
+script_name=$( echo "$0" | cut -d "." -f1 )
+logfile="$log_folder/$script_name.log"
+
+if [ $user_id -ne 0 ]; then
+    echo "need root priviliges"
+    exit 1;
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 is failure" | tee -a $logfile
+    else
+        echo "installing $2 is success" | tee -a $logfile
+    fi
+}
+
+
